0cd3efb846da16f5b1976ca313eff76e795ceb83
ibx34 <Guamjust@gmail.com>
2021-06-01 09:39:37 -0400

    Yes

--- core/parser/parser_utils.cpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-using namespace std;
-
-InvalidSyntaxError create_syntax_error(shared_ptr<ParserStore> parser_store, string exp, int extra = 0) {
-    InvalidSyntaxError e(parser_store->current_t->start, parser_store->current_t->end, "Expected " + exp);
-    e.extra = extra;
-    return e;
-}
--- core/utils/basic_utils.cpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-using namespace std;
-
-bool in_array(const string &value, const vector<string> &array){
-    return find(array.begin(), array.end(), value) != array.end();
-}
-
